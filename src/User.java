eb8d8e145e16b0150abb3037f2d91628a6fdabb6
sdeng238 <shirleyd2023@student.cis.edu.hk>
2023-03-05 00:24:22 +0800

    done & tested

--- src/User.java
+++ src/User.java
@@ -17,6 +17,14 @@ public class User {
         return progress;
     }
 
+    @Override
+    public String toString() {
+        return "User{" +
+                "rank=" + rank +
+                ", progress=" + progress +
+                '}';
+    }
+
     public void incProgress(int activityRank)
     {
         //throw exception if activity rank <-8, =0 or >8
@@ -27,10 +35,32 @@ public class User {
 
         //check if zero exists between user's rank and activity's rank
         boolean zeroExists = false;
-        //if user's rank is higher than activity's rank by 1, add 1 point
-        if(rank - activityRank == 1)
+        int difference;
+        //if user's rank is higher than activity's, check whether range includes 0
+        if(activityRank < rank)
         {
-            progress++;
+            //check if range between user's rank and activity's rank has 0
+            for(int num = activityRank; num <= rank; num++)
+            {
+                if(num == 0)
+                {
+                    zeroExists = true;
+                }
+            }
+
+            if(zeroExists)
+            {
+                difference = rank - activityRank - 1;
+            }
+            else
+            {
+                difference = rank - activityRank;
+            }
+
+            if(difference == 1)
+            {
+                progress++;
+            }
         }
         //if user's rank is equal to activity's rank, add 3 points
         else if(rank == activityRank)
@@ -63,10 +93,11 @@ public class User {
         //if the updated progress is larger than or equals to 100 points
         if(progress >= 100)
         {
-            //edit the progress points to the difference between the updated progress and the nearest smaller multiple of 100
-            progress = progress - 100 * (progress / 100);
+
             //increase the user's rank by the number of multiples of 100 subtracted from the updated progress
             rank = rank + (progress / 100);
+            //edit the progress points to the difference between the updated progress and the nearest smaller multiple of 100
+            progress = progress - 100 * (progress / 100);
             //if the user's updated rank is 0, increase to 1
             if(rank == 0)
             {
